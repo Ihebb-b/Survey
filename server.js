5cc4d0ef868353bc5866332bac539ab8b4a4d783
Jihen Atig <jihen.atig@sqli-services.com>
2024-10-10 10:35:32 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const surveyRoutes = require('./routes/surveyRoutes');
+const statisticsRoutes = require('./routes/statisticsRoutes');
+
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// Connect Database
+connectDB();
+
+// Routes
+app.use('/api/surveys', surveyRoutes);
+app.use('/api/statistics', statisticsRoutes); // Register statistics route
+
+// Set up a root route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Starting the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
